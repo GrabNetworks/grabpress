1078daf2a9edd7931bf79d1a656399d06b2dc134
Ross Sclafani <ross@sclafani.net>
2012-09-12 11:40:15 -0400

    clear link form on Cancel

--- src/includes/account/forms/link.php
+++ src/includes/account/forms/link.php
@@ -3,8 +3,8 @@
 	<form id="link-existing" method="post" action="">
 		<table>
 			<input type="hidden" name="action" id="action" value="link-user"/>
-			<tr><td>Email address<input name="email" id="id_email" type="text" value="<?php echo $_POST['email'] !== NULL ? $_POST['email'] : '';?>" /></td></tr>
-			<tr><td>Password<input name="password" id="id_password" type="password"/></td></tr>
+			<tr><td>Email address<input name="email" id="email" type="text" value="<?php echo $_POST['email'] !== NULL ? $_POST['email'] : '';?>" /></td></tr>
+			<tr><td>Password<input name="password" id="password" type="password"/></td></tr>
 			<tr><td class = "account-help">
 					<a href="#">I don't remember my password</a>
 					<input type="button" class="button-primary" disabled="disabled" id="submit_button" value="<?php _e( ($_POST[ 'action' ] == 'switch' ? 'Change' : 'Link').' Account') ?>"/>
@@ -22,9 +22,9 @@
 			}
 			function validate(){
 				console.log( 'validate');
-				var email_valid =  $( '#id_email' ).val().match(/[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i );
+				var email_valid =  $( '#email' ).val().match(/[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i );
 				console.log( 'email:' + email_valid );
-				var pass_valid = ( $('#id_password').val().length > 0 ) ;
+				var pass_valid = ( $('#password').val().length > 0 ) ;
 				console.log( 'pass:' + pass_valid );
 			
 				var valid = email_valid && pass_valid;
@@ -50,6 +50,8 @@
 			
 			$('#cancel_button').click(function(){
 				$('#action').val('default');
+				$('#email').val('');
+				$('#password').val('');
 				$('#link-existing').submit();
 			})
 		})( jQuery )
