fd5a01733d8812f4b66d4f857e51a67106fac0d4
John Duquette <jduquette@grabnetworks.com>
2012-09-21 12:39:28 -0400

    Updated to 0.6.0b71

--- src/GrabPress.php
+++ src/GrabPress.php
@@ -3,7 +3,7 @@
 Plugin Name: GrabPress
 Plugin URI: http://www.grab-media.com/publisher/solutions/autoposter
 Description: Configure Grab's AutoPoster software to deliver fresh video direct to your Blog. Create or use an existing Grab Media Publisher account to get paid!
-Version: 0.6.0b69
+Version: 0.6.0b71
 Author: Grab Media
 Author URI: http://www.grab-media.com
 License: GPL2
@@ -25,7 +25,7 @@ License: GPL2
 */
 if ( ! class_exists( 'GrabPress' ) ) {
 	class GrabPress {
-		static $version = '0.6.0b69';
+		static $version = '0.6.0b71';
 		static $api_key;
 		static $invalid = false;
 		static $environment = 'grabqa'; // or 'grabnetworks'
