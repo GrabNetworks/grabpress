f9077932245ccf62d79bfbcf0b4d2d43c1f6c4e6
Carolina <carol.sancos@gmail.com>
2012-12-20 17:34:56 -0300

    AUTO-AUTO-434 As a WP editor, I'd like the preview image for each Autopost to be added to the post's Featured Image

--- src/GrabPress.php
+++ src/GrabPress.php
@@ -1367,7 +1367,34 @@ if ( ! class_exists( 'GrabPress' ) ) {
 						"post_type" => "post",
 						"post_status" => "draft",
 						"tags_input" => $item->mediagroup->mediakeywords
-					));
+					));				
+
+					$upload_dir = wp_upload_dir();
+					$image_url = $item->mediagroup->mediathumbnail[1]->attributes()->url;
+					$image_data = file_get_contents($image_url);
+					$filename = basename($image_url);
+					if(wp_mkdir_p($upload_dir['path']))
+					    $file = $upload_dir['path'] . '/' . $filename;
+					else
+					    $file = $upload_dir['basedir'] . '/' . $filename;
+					file_put_contents($file, $image_data);
+
+					$wp_filetype = wp_check_filetype($filename, null );
+					$attachment = array(
+						'guid' => sanitize_file_name($filename),
+						'guid' => "endworld",
+					    'post_mime_type' => $wp_filetype['type'],
+					    'post_title' => sanitize_file_name($filename),
+					    'post_content' => '',
+					    'post_status' => 'inherit'
+					);
+					$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+					require_once(ABSPATH . 'wp-admin/includes/image.php');
+					$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+					wp_update_attachment_metadata( $attach_id, $attach_data );
+
+					set_post_thumbnail( $post_id, $attach_id );
+
 					echo json_encode(array(
 						"status" => "redirect", 
 						"url" => "post.php?post=".$post_id."&action=edit"));
