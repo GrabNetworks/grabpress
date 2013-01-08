9360c20fc1b6f792b67c7ee9b09152ce297ae314
Carolina <carol.sancos@gmail.com>
2013-01-08 12:51:45 -0300

    493 [WP 3.5] Catalog > Create post button is not working.

--- src/GrabPress.php
+++ src/GrabPress.php
@@ -204,21 +204,26 @@ if ( ! class_exists( 'GrabPress' ) ) {
 			if(!GrabPress::$player_settings){
 				$settings_json =  GrabPress::api_call( 'GET',  '/connectors/'.GrabPress::get_connector_id().'/player_settings?api_key='.GrabPress::$api_key );
 				$settings = json_decode( $settings_json );
-
-				if(isset($settings->error)&& $settings->error->status_code == 404){//nonexistent. set defaults.
+				
+				if( empty($settings) || (isset($settings->error) && $settings->error->status_code == 404)){//nonexistent. set defaults.
 					GrabPress::$player_settings = array();
 				}else{
-					GrabPress::$player_settings = $settings->player_setting;
+					GrabPress::$player_settings = array(
+						"width" => $settings->player_setting->width,
+					 	"height" => $settings->player_setting->height,
+					 	"ratio" => $settings->player_setting->ratio
+					 	);
 				}
+				
 			}
 			
 			return GrabPress::$player_settings;
 		}
 		static function get_player_settings_for_embed(){
 			$sett = GrabPress::get_player_settings();
-			$defaults = array("width" => 600, "height"=> 270);
+			$defaults = array("width" => 600, "height"=> 270, "ratio" => "16:9");
 
-			return array_merge($defaults, array("width" => $sett->width, "height" => $sett->height));
+			return array_merge($defaults, $sett);
 		}
 
 		static function get_connector() {
@@ -910,9 +915,9 @@ if ( ! class_exists( 'GrabPress' ) ) {
 				$player = GrabPress::get_player_settings();
 
 				if($player){
-					$settings["width"] = $player->width;
-					$settings["height"] = $player->height;
-					$settings["ratio"] = $player->ratio=="16:9"?"widescreen":"standard";
+					$settings["width"] = $player["width"];
+					$settings["height"] = $player["height"];
+					$settings["ratio"] = $player["ratio"]=="16:9"?"widescreen":"standard";
 					$settings["action"] = "edit";
 				}
 
