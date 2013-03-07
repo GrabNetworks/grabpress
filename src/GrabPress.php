ae840bb2cf5ccd55d35adf461f84426c5f343604
John Duquette <jduquette@grabnetworks.com>
2013-03-07 17:11:51 -0500

    Another merge conflict

--- src/GrabPress.php
+++ src/GrabPress.php
@@ -665,12 +665,7 @@ if ( ! class_exists( 'GrabPress' ) ) {
 			wp_enqueue_style( 'nanoscroller', $plugin_url.'/css/nanoscroller.css');		
 			wp_enqueue_style( 'grabpress-css', $plugin_url.'/css/grabpress.css' , array("jquery-ui-theme", "bootstrap", "nanoscroller"));			
 			
-<<<<<<< HEAD
-			wp_enqueue_style( 'grabpresss-fonts', "http://fast.fonts.com/cssapi/7ece15ec-35ef-4a92-bc79-b5349675eb23.css");
-=======
-			
 			wp_enqueue_style( 'grabpresss-fonts', "http://images.grab-media.com/widgets/grab-fonts/fonts.css");
->>>>>>> integration
 			wp_enqueue_style( 'bootstrap-responsive', $plugin_url.'/css/bootstrap-responsive.css' );
 			
 		}
