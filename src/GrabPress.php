855e154c6fa884143fa8292a75126d1ccda69e35
John Duquette <jduquette@grabnetworks.com>
2013-03-14 09:36:00 -0400

    Versioning and setting to QA environment

--- src/GrabPress.php
+++ src/GrabPress.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__)."/GrabPressAPI.php";
 Plugin Name: GrabPress
 Plugin URI: http://www.grab-media.com/publisher/grabpress
 Description: Configure Grab's AutoPoster software to deliver fresh video direct to your Blog. Link a Grab Media Publisher account to get paid!
-Version: 2.2.0
+Version: 2.2.0-03142013
 Author: Grab Media
 Author URI: http://www.grab-media.com
 License: GPL2
@@ -27,10 +27,10 @@ License: GPL2
 */
 if ( ! class_exists( 'GrabPress' ) ) {
 	class GrabPress {
-		static $version = '2.2.0';
+		static $version = '2.2.0-03141013';
 		static $api_key;
 		static $invalid = false;
-		static $environment =  'grabnetworks';
+		static $environment =  'grabqa';
 		static $debug = true;
 		static $message = false;
 		static $error = false;
