e77b82d528808cd957aca57718a53f911730c136
oana <oana.crisan@3pillarglobal.com>
2013-06-11 14:42:31 +0300

    AUTO-881 : fix for merge

--- src/js/autoposter.js
+++ src/js/autoposter.js
@@ -16,7 +16,7 @@ var GrabPressAutoposter = GrabPressAutoposter || {
     /* Modal preview window definition and its closing behavior  */
     previewdialogConf : {
         modal: true,
-        width:900,
+        width:910,
         height:jQuery(window).height(),
         draggable: false,
         close: function(){
